17799fffaa77cebefcc686c10d44124649f29666
Andany <AndanyEatsWings@gmail.com>
2015-06-09 09:40:44 -0400

    cleaned up code a little

--- src/com/chess_game/ChessGame.java
+++ src/com/chess_game/ChessGame.java
@@ -12,14 +12,14 @@ import java.util.*;
 
 public class ChessGame {
   public static void main(String[] args) {
-    run();
-  }
-  
-  public static void run() {
-    //Pre:
-    //Post:
-    addPieces();
-    makeBoard();
+    //Pre: None
+    //Post: Runs the game
+    
+    //makes the pieces neccessary for the game
+    com.chess_game.elements.Constants.makePieces();
+    
+    //makes the board, with the pieces contained on the board
+    com.chess_game.elements.Constants.makeBoard();
     
     do {
       System.out.println(com.chess_game.elements.Constants.game_board);
@@ -36,71 +36,7 @@ public class ChessGame {
     } while (true);
     
   }
-  
-  public static void addPieces() {
-    //Pre: None
-    //Post: Creates all pieces for the game
     
-    //add kings
-    com.chess_game.elements.Constants.pieces.put("white_king", new King('W'));
-    com.chess_game.elements.Constants.pieces.put ("black_king", new King('B'));
-    
-    //add kings
-    com.chess_game.elements.Constants.pieces.put("white_queen", new Queen('W'));
-    com.chess_game.elements.Constants.pieces.put ("black_queen", new Queen('B'));
-    
-    //add rooks
-    for (int i = 0; i < 2; i++)
-      com.chess_game.elements.Constants.pieces.put("white_rook" + String.valueOf(i), new Rook('W'));
-    for (int i = 0; i < 2; i++)
-      com.chess_game.elements.Constants.pieces.put("black_rook" + String.valueOf(i), new Rook('B'));
-    
-    //add knights 
-    for (int i = 0; i < 2; i++)
-      com.chess_game.elements.Constants.pieces.put("white_knight" + String.valueOf(i), new Knight('W'));
-    for (int i = 0; i < 2; i++)
-      com.chess_game.elements.Constants.pieces.put("black_knight" + String.valueOf(i), new Knight('B'));
-    
-    //add bishops
-    for (int i = 0; i < 2; i++)
-      com.chess_game.elements.Constants.pieces.put("white_bishop" + String.valueOf(i), new Bishop('W'));
-    for (int i = 0; i < 2; i++)
-      com.chess_game.elements.Constants.pieces.put("black_bishop" + String.valueOf(i), new Bishop('B'));
-    
-    //add pawns
-    for (int i = 0; i < 8; i++)
-      com.chess_game.elements.Constants.pieces.put("white_pawn" + String.valueOf(i), new Pawn('W'));
-    for (int i = 0; i < 8; i++)
-      com.chess_game.elements.Constants.pieces.put("black_pawn" + String.valueOf(i), new Pawn('B'));
-  }
-  
-  public static void makeBoard() {
-    //Pre: None
-    //Post: Makes the actual game board
-    Box[][] b = new Box[8][8];
-    
-    String[] str = {"rook0", "knight0", "bishop0", "queen", "king", "bishop1", "knight1", "rook1"};
-    
-    for (int i = 0; i < 8; i++)
-      b[7][i] = new Box(7, i, com.chess_game.elements.Constants.pieces.get("white_" + str[i]));
-    
-    for (int i = 0; i < 8; i++)
-      b[0][i] = new Box(0, i, com.chess_game.elements.Constants.pieces.get("black_" + str[i]));
-    
-    for (int i = 0; i < 8; i++)
-      b[6][i] = new Box(6, i, com.chess_game.elements.Constants.pieces.get("white_pawn" + String.valueOf(i)));
-    
-    for (int i = 0; i < 8; i++)
-      b[1][i] = new Box(1, i, com.chess_game.elements.Constants.pieces.get("black_pawn" + String.valueOf(i)));
-    
-    for (int i = 2; i < 6; i++) {
-      for (int j = 0; j < 8; j++)
-        b[i][j] = new Box(i, j);
-    }
-    
-    com.chess_game.elements.Constants.game_board = new Board(b);
-  }
-  
   public static boolean player1Turn() {
     //Pre: None
     //Post: Executes white's turn
@@ -184,7 +120,7 @@ public class ChessGame {
     }
     //else
     
-    //turns user input into coordinates we, programers, can work with, and stores it in an array
+    //turns user input into coordinates we, programmers, can work with, and stores it in an array
     Coordinate[] c = {new Coordinate(com.chess_game.elements.Constants.x_coord.get(move.charAt(0)) - 1, 8 - Integer.parseInt(move.substring(1, 2))), 
       new Coordinate(com.chess_game.elements.Constants.x_coord.get(move.charAt(2)) - 1, 8 - Integer.parseInt(move.substring(3, 4)))};
     
--- src/com/chess_game/elements/Constants.java
+++ src/com/chess_game/elements/Constants.java
@@ -24,4 +24,69 @@ public class Constants {
     put('g', 7);
     put('h', 8);
   }};
+  
+  public static void makePieces() {
+    //Pre: None
+    //Post: Creates all pieces for the game
+    
+    //make kings
+    com.chess_game.elements.Constants.pieces.put("white_king", new King('W'));
+    com.chess_game.elements.Constants.pieces.put ("black_king", new King('B'));
+    
+    //make kings
+    com.chess_game.elements.Constants.pieces.put("white_queen", new Queen('W'));
+    com.chess_game.elements.Constants.pieces.put ("black_queen", new Queen('B'));
+    
+    //make rooks
+    for (int i = 0; i < 2; i++)
+      com.chess_game.elements.Constants.pieces.put("white_rook" + String.valueOf(i), new Rook('W'));
+    for (int i = 0; i < 2; i++)
+      com.chess_game.elements.Constants.pieces.put("black_rook" + String.valueOf(i), new Rook('B'));
+    
+    //make knights 
+    for (int i = 0; i < 2; i++)
+      com.chess_game.elements.Constants.pieces.put("white_knight" + String.valueOf(i), new Knight('W'));
+    for (int i = 0; i < 2; i++)
+      com.chess_game.elements.Constants.pieces.put("black_knight" + String.valueOf(i), new Knight('B'));
+    
+    //make bishops
+    for (int i = 0; i < 2; i++)
+      com.chess_game.elements.Constants.pieces.put("white_bishop" + String.valueOf(i), new Bishop('W'));
+    for (int i = 0; i < 2; i++)
+      com.chess_game.elements.Constants.pieces.put("black_bishop" + String.valueOf(i), new Bishop('B'));
+    
+    //make pawns
+    for (int i = 0; i < 8; i++)
+      com.chess_game.elements.Constants.pieces.put("white_pawn" + String.valueOf(i), new Pawn('W'));
+    for (int i = 0; i < 8; i++)
+      com.chess_game.elements.Constants.pieces.put("black_pawn" + String.valueOf(i), new Pawn('B'));
+  }
+  
+  public static void makeBoard() {
+    //Pre: None
+    //Post: Makes the actual game board
+    Box[][] b = new Box[8][8];
+    
+    String[] str = {"rook0", "knight0", "bishop0", "queen", "king", "bishop1", "knight1", "rook1"};
+    
+    for (int i = 0; i < 8; i++)
+      b[7][i] = new Box(7, i, com.chess_game.elements.Constants.pieces.get("white_" + str[i]));
+    
+    for (int i = 0; i < 8; i++)
+      b[0][i] = new Box(0, i, com.chess_game.elements.Constants.pieces.get("black_" + str[i]));
+    
+    for (int i = 0; i < 8; i++)
+      b[6][i] = new Box(6, i, com.chess_game.elements.Constants.pieces.get("white_pawn" + String.valueOf(i)));
+    
+    for (int i = 0; i < 8; i++)
+      b[1][i] = new Box(1, i, com.chess_game.elements.Constants.pieces.get("black_pawn" + String.valueOf(i)));
+    
+    for (int i = 2; i < 6; i++) {
+      for (int j = 0; j < 8; j++)
+        b[i][j] = new Box(i, j);
+    }
+    
+    com.chess_game.elements.Constants.game_board = new Board(b);
+  }
+
 }
